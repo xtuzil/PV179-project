afacfd62965cbf5dc25bdd14198d47319f868986
Koala <skywingsfs@gmail.com>
2021-09-24 12:56:31 +0200

    Setup basic project skeleton

---
+++ PV179 Project/PV179 Project/Program.cs
@@ -0,0 +1,18 @@
+﻿using Data_Access_Layer;
+using System;
+
+namespace PV179_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new CactusesManagerDbContext("Server=(localdb)\\mssqllocaldb;Integrated Security=True;MultipleActiveResultSets=True;Database=CactusesManager;Trusted_Connection=True;"))
+            {
+                db.Database.EnsureCreated();
+                db.Cactuses.Add(new Data_Access_Layer.Models.Cactus() { });
+                db.SaveChanges();
+            }
+        }
+    }
+}
