81f67ea357263e9db3815b6ee44ddd885b55c05e
Kis Lázár Bence <akislb99@gmail.com>
2021-10-16 21:31:51 +0200

    UoW and Repository refinements

--- DAL/Infrastructure/UnitOfWork/IUnitOfWork.cs
+++ DAL/Infrastructure/UnitOfWork/IUnitOfWork.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CactusDAL.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,20 @@ namespace CactusDAL.UnitOfWork
 {
     public interface IUnitOfWork
     {
+        IRepository<PostalAddress> Addresses { get; }
+        IRepository<Cactus> Cactuses { get; }
+        IRepository<CactusOffer> CactusOffers { get; }
+        IRepository<CactusRequest> CactusRequests { get; }
+        IRepository<Comment> Comments { get; }
+        IRepository<Genus> Genuses { get; }
+        IRepository<Offer> Offers { get; }
+        IRepository<Review> Reviews { get; }
+        IRepository<Report> Reports { get; }
+        IRepository<Species> Species { get; }
+        IRepository<Transfer> Transfers { get; }
+        IRepository<User> Users { get; }
+        IRepository<Wishlist> WishlistItems { get; }
+
         void Commit();
         void RegisterAction(Action action);
         void Dispose();
--- DAL/Infrastructure/UnitOfWork/UnitOfWorkBase.cs
+++ DAL/Infrastructure/UnitOfWork/UnitOfWorkBase.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CactusDAL.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +9,21 @@ namespace CactusDAL.UnitOfWork
 {
     public abstract class UnitOfWorkBase : IUnitOfWork, IDisposable
     {
-        private List<Action> _afterCommitActions { get; set; }
+        private List<Action> _afterCommitActions { get; set; } = new List<Action>();
+        public abstract IRepository<PostalAddress> Addresses { get; }
+        public abstract IRepository<Cactus> Cactuses { get; }
+        public abstract IRepository<CactusOffer> CactusOffers { get; }
+        public abstract IRepository<CactusRequest> CactusRequests { get; }
+        public abstract IRepository<Comment> Comments { get; }
+        public abstract IRepository<Genus> Genuses { get; }
+        public abstract IRepository<Offer> Offers { get; }
+        public abstract IRepository<Review> Reviews { get; }
+        public abstract IRepository<Report> Reports { get; }
+        public abstract IRepository<Species> Species { get; }
+        public abstract IRepository<Transfer> Transfers { get; }
+        public abstract IRepository<User> Users { get; }
+        public abstract IRepository<Wishlist> WishlistItems { get; }
+
         public void Commit()
         {
             foreach (Action action in _afterCommitActions)
