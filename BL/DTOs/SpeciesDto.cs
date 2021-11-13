46e2fac6e0773f228fc929da78d9f85ab6a13be9
Koala <skywingsfs@gmail.com>
2021-11-13 19:41:24 +0100

    Add id to SpeciesDto

--- BL/DTOs/SpeciesDto.cs
+++ BL/DTOs/SpeciesDto.cs
@@ -8,8 +8,8 @@ namespace BL.DTOs
 {
     public class SpeciesDto
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public string LatinName { get; set; }
-
     }
 }
