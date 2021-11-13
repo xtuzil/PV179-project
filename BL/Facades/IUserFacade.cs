50b9d9d4fb05a4f65a74c328fe3570da8e36c7ae
Matěj Tužil <matestu@seznam.cz>
2021-11-13 21:33:07 +0100

    user facade interface fix

--- BL/Facades/IUserFacade.cs
+++ BL/Facades/IUserFacade.cs
@@ -10,12 +10,12 @@ namespace BL.Facades
     public interface IUserFacade
     {
         public Task<List<UserInfoDto>> GetAllUserWithNameAsync(string name);
-        public Task<List<UserInfoDto>> GetUserInfo(int userId);
+        public Task<UserInfoDto> GetUserInfo(int userId);
         public Task<List<ReviewDto>> GetUserReviews(int userId);
         public Task<List<ReviewDto>> GetReviewsOnUser(int userId);
         public void UpdateUserInfo(UserUpdateDto user);
         public Task<List<OfferDto>> GetUserOffers(int userId);
-        public Task<List<OfferDto>>  GetUserReceivedOffers(int userId);
+        public Task<List<OfferDto>> GetUserReceivedOffers(int userId);
         public Task<List<TransferDto>> GetUserTransfers(int userId);
     }
 }
