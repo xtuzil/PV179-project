3a34d797f73af79037d7a36fbf1aa99af6e643a1
Koala <skywingsfs@gmail.com>
2021-11-13 20:48:12 +0100

    Fix Cactus offers/request DTOs

--- BL/Facades/IOfferFacade.cs
+++ BL/Facades/IOfferFacade.cs
@@ -9,8 +9,8 @@ namespace BL.Facades
 {
     public interface IOfferFacade
     {
-        public OfferDto CreateOffer(OfferCreateDto offer, List<CactusOfferDto> offeredCactuses, List<CactusRequestDto> requestedCactuses);
-        public OfferDto CreateCounterOffer(OfferCreateDto offer, List<CactusOfferDto> offeredCactuses, List<CactusRequestDto> requestedCactuses, int previousOfferId);
+        public OfferDto CreateOffer(OfferCreateDto offer, List<CactusOfferCreateDto> offeredCactuses, List<CactusRequestCreateDto> requestedCactuses);
+        public OfferDto CreateCounterOffer(OfferCreateDto offer, List<CactusOfferCreateDto> offeredCactuses, List<CactusRequestCreateDto> requestedCactuses, int previousOfferId);
         public void AcceptOffer(int offerId);
         public void RejectOffer(int offerId);
     }
