0109364ea565ccb0a4c3c607fa23627aa1f6afa1
Koala <skywingsfs@gmail.com>
2021-11-13 15:43:11 +0100

    Forgot to add a newly created DTO

---
+++ BL/DTOs/CactusCreateDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.DTOs
+{
+    public class CactusCreateDto
+    {
+        [Required]
+        public int OwnerId { get; set; }
+        [Required]
+        public int SpeciesId { get; set; }
+
+        public bool ForSale { get; set; }
+
+        public DateTime SowingDate { get; set; }
+        public int PotSize { get; set; }
+        public int Amount { get; set; }
+
+        public string Note { get; set; }
+    }
+}
