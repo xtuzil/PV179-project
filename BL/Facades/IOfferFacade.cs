028e4f9716bdc1b937a553efb56aab754b903177
Koala <skywingsfs@gmail.com>
2021-11-13 21:07:22 +0100

    Improve Offer facade interface

--- BL/Facades/IOfferFacade.cs
+++ BL/Facades/IOfferFacade.cs
@@ -9,8 +9,8 @@ namespace BL.Facades
 {
     public interface IOfferFacade
     {
-        public OfferDto CreateOffer(OfferCreateDto offer, List<CactusDto> offeredCactuses, List<CactusDto> requestedCactuses);
-        public OfferDto CreateCounterOffer(OfferCreateDto offer, List<CactusDto> offeredCactuses, List<CactusDto> requestedCactuses, int previousOfferId);
+        public OfferDto CreateOffer(OfferCreateDto offer);
+        public OfferDto CreateCounterOffer(OfferCreateDto offer, int previousOfferId);
         public void AcceptOffer(int offerId);
         public void RejectOffer(int offerId);
     }
