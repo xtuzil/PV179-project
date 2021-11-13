d09e0e8c5520203ac788bd3e6f1dd0c6fc8da612
Koala <skywingsfs@gmail.com>
2021-11-13 19:55:28 +0100

    Add Transfer facade interface

---
+++ BL/Facades/ITransferFacade.cs
@@ -0,0 +1,15 @@
+﻿using BL.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Facades
+{
+    public interface ITransferFacade
+    {
+        public Task<List<ReviewDto>> GetTransferReviews(int transferId);
+        public void ProcessTransfer(int transferId);
+    }
+}
