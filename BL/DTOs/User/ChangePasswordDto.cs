af5fb096ab4aa370ffd6a52c93a60b5a5ab0d402
Koala <skywingsfs@gmail.com>
2021-12-14 02:45:32 +0100

    Add validation to ChangePasswordDto

--- BL/DTOs/User/ChangePasswordDto.cs
+++ BL/DTOs/User/ChangePasswordDto.cs
@@ -11,6 +11,8 @@ namespace BL.DTOs
     {
         [Required]
         public int Id { get; set; }
+        [Required]
+        [StringLength(100, MinimumLength = 8)]
         public string Password { get; set; }
         [Compare("Password", ErrorMessage = "Password confirmation does not match. Try again.")]
         public string PasswordConfirmation { get; set; }
