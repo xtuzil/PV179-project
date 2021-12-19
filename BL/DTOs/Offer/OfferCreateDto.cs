5b7d6cf0dfd24e34fd5fba5c1d42891b66a9bbce
Kis Lázár Bence <akislb99@gmail.com>
2021-12-19 17:56:11 +0100

    Label updates

--- BL/DTOs/Offer/OfferCreateDto.cs
+++ BL/DTOs/Offer/OfferCreateDto.cs
@@ -11,9 +11,9 @@ namespace BL.DTOs
         public int AuthorId { get; set; }
         [Required]
         public int RecipientId { get; set; }
-        [Range(0, int.MaxValue, ErrorMessage = "{0} can't be negative.")]
+        [Range(0, int.MaxValue, ErrorMessage = "Offered money cannot be negative.")]
         public double? OfferedMoney { get; set; }
-        [Range(0, int.MaxValue, ErrorMessage = "{0} can't be negative.")]
+        [Range(0, int.MaxValue, ErrorMessage = "Requested money cannot be negative.")]
         public double? RequestedMoney { get; set; }
 
         public int? PreviousOfferId { get; set; }
